15b600c05c66189193829a89756a9c2ade7521ab
Alec Liu <sammiliu71@gmail.com>
2025-06-16 12:12:32 -0700

    Added title collection

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 const puppeteer = require('puppeteer');
-
-console.log('hello 4');
+const URL = 'https://archiveofourown.org/tags/Sung%20Jin-Woo*s*Woo%20Jin-Chul/works';
 
 async function pleaseStart() {
-    console.log('hello 1');
     
     const browser = await puppeteer.launch({
-        // executablePath: "C:\Users\sammi\AppData\Local\Programs\Opera GX\opera.exe",
-        enableExtensions: true,
-        headless: false,
-        dumpio: true
+        // headless: false //| headless: false means that the browser will pop up when launched, makes it visible for the programmer
     });
 
-    console.log('hello 3');
     const page = await browser.newPage();
-    console.log('hello 2');
-    await page.goto('https://quotes.toscrape.com');
+    await page.goto(URL);
+    
+    //takes a screenshot and saves it as quotes.png in the current project folder
+    await page.screenshot({
+        path: 'fics.png'
+    });
+
+    // prints out the html content of the page
+    // console.log(await page.content());
+
+    const fics = await page.evaluate(() => {
+        const ficElements = document.querySelectorAll('[role="article"]');
+
+        const ficsArray = [];
+
+        for(const ficElement of ficElements) {
+            const title = ficElement.querySelector('.header.module .heading');
+
+            const clean = title.textContent.replace(/^(?=\n)$|^\s*|\s*$|\n\n+/gm, "");
+            const final = clean.replace(/\n/g, " ");
+
+            ficsArray.push(final);
+        }
+
+        return ficsArray;
+    });
+
+    console.log(fics);
+    
+    // prints out the html content of the page
+    // console.log(await page.content());
+    
+    await browser.close();
 }
 
 pleaseStart();
